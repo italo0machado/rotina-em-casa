3c7ead497d811028369b75e653a1ccc2abf59fac
Clarice <italo0machado@gmail.com>
2026-06-06 18:12:22 -0300

    fix: configuração mínima do Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['@blossom-carousel/react', '@blossom-carousel/core']
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()]
 })
